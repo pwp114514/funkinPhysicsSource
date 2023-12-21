aa8d827b471aadfc0f12346337d778ad472ac982
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-12-21 21:39:23 +0800

    Add files via upload

--- assets/preload/data/absurde/spaceMashMechanics.lua
+++ assets/preload/data/absurde/spaceMashMechanics.lua
@@ -8,11 +8,22 @@ function opponentNoteHit(membersIndex, noteData, noteType, isSustainNote)
     end
 end
 
+function onCreatePost()
+	makeLuaText('tipsandroid', 'Click Screen !', 0, 0, 0)
+	addLuaText('tipsandroid')
+	setTextSize('tipsandroid', 30)
+	screenCenter('tipsandroid')
+	setProperty('tipsandroid.y', screenHeight - getProperty('tipsandroid.height') - 50)
+	setObjectCamera('tipsandroid', 'camOther')
+end
+
 function onUpdate(elapsed)
     if getProperty('spaceMashImage.alpha') >= 1 then
         setHealth(0)
     end
-    if keyJustPressed('space') then
+    if keyJustPressed('space') or mouseClicked('left') then
         setProperty('spaceMashImage.alpha', getProperty('spaceMashImage.alpha') - 0.05)
     end
+    
+    setProperty('tipsandroid.alpha', getProperty('spaceMashImage.alpha'))
 end
